f853fcb1aec34617e0296ef152484b87f765f184
JosephStewart22 <126738987+JosephStewart22@users.noreply.github.com>
2023-05-17 16:44:04 -0600

    added rating POST

--- flatiron-games/src/components/GameCard.js
+++ flatiron-games/src/components/GameCard.js
@@ -1,10 +1,19 @@
 import React, {useState} from "react";
+import gameKey from "../APIKey";
 
-function GameCard({ game, handleDetailClick, handleRatingClick}) {
+function GameCard({ game, handleDetailClick, handleRating}) {
 const [userRating, setUserRating] = useState(false)
 
 function handleRatingClick() {
-  setUserRating((userRating) => !userRating)
+  fetch(`https://api.rawg.io/api/ratings?key=${gameKey}`, {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userRating),
+  })
+  .then(r => r.json())
+  .then(userRating => setUserRating(userRating))
 }
   return (
     <div class="card">
@@ -15,14 +24,13 @@ function handleRatingClick() {
         <p>{platform.platform.name}</p>
       ))}
       <p>{game.rating}/{game.rating_top}</p>
-      <button onClick={handleRatingClick}>Add Rating</button>
       <form>
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          <button newRating={userRating} onClick={handleRatingClick(userRating)}>1</button>
+          <button newRating={userRating} onClick={handleRatingClick(userRating)}>2</button>
+          <button newRating={userRating} onClick={handleRatingClick(userRating)}>3</button>
+          <button newRating={userRating} onClick={handleRatingClick(userRating)}>4</button>
+          <button newRating={userRating} onClick={handleRatingClick(userRating)}>5</button>
         </div>
       </form>
       <button onClick={() => handleDetailClick(game)} class="detailButton">
