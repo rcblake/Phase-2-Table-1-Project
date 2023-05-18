63f9fc50f3b0b7b3ac113fa6a098a714898e7aee
rcblake <127946751+rcblake@users.noreply.github.com>
2023-05-18 09:57:20 -0700

    remove body

--- flatiron-games/src/components/Header.js
+++ flatiron-games/src/components/Header.js
@@ -1,14 +1,8 @@
 function Header() {
   return (
-    <div>
-      <div id="siteTitle">
-        <h1>Flatiron Games</h1>
-      </div>
-      <div id="userHeading">
-        <h3>Login/Greeting</h3>
-        <img src="" alt="" />
-      </div>
-    </div>
+    <>
+      <h1 className="headerTitle">Flatiron Games</h1>
+    </>
   );
 }
 
