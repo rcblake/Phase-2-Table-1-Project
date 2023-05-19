12ae16ccbb4e6c27f78b125518931c97eb3b6648
rcblake <127946751+rcblake@users.noreply.github.com>
2023-05-18 18:11:59 -0700

    modal sytling

--- flatiron-games/src/components/DetailModal.js
+++ flatiron-games/src/components/DetailModal.js
@@ -9,24 +9,27 @@ function DetailModal({ modalGame, handleModalClose }) {
 
   return (
     <div class="modal">
-      <div className="detailTitle">
+      <div className="modalHeader">
         <h2>{modalGame.name}</h2>
       </div>
-      <img class="modalMainImage" src={detailImage} alt={modalGame.name} />
-      <div class="modalText">
-        <h4>Genres 2 Max</h4>
-        <h4>Release Year</h4>
-        <h4>
-          {modalGame.rating} / {modalGame.rating_top}
-          &#11088
-        </h4>
-        <h4>ESRB</h4>
-        <div>My Rating</div>
+      <div className="modalBody">
+        <img class="modalMainImage" src={detailImage} alt={modalGame.name} />
+        <div class="modalText">
+          <h4>Genres 2 Max</h4>
+          <h4>Release Year</h4>
+          <h4>
+            {modalGame.rating} / {modalGame.rating_top}
+            &#11088
+          </h4>
+          <h4>ESRB</h4>
+          <div>My Rating</div>
+        </div>
       </div>
 
       <div>
         {modalGame.short_screenshots.map((screenshot) => (
           <img
+            className="modalScreenShots"
             src={screenshot.image}
             alt="gameplay screenshot"
             onClick={handleDetailImg}
